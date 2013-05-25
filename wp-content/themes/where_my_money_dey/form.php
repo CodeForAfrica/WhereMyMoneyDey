6547e510e157343cfbdb67312d7f723fbe65248d
Anne Muigai <anne.muigai@gmail.com>
2013-05-25 20:27:02 +0300

    Mainly UI changes, with 'royalties paid for', actNOW, and speaker profiles sections changed

---
+++ wp-content/themes/where_my_money_dey/form.php
@@ -0,0 +1,32 @@
+<form action="https://docs.google.com/a/openinstitute.com/spreadsheet/formResponse?formkey=dFk3T28taG83ODNYUjVFOTlaRm1UY0E6MQ&amp;ifq" method="POST" id="ss-form">
+<div class="story">
+
+<br>
+<!-- <div class="errorbox-good">
+<div class="ss-item ss-item-required ss-text"><div class="ss-form-entry"> --><label class="ss-q-title" for="entry_0">Your name: 
+<span class="ss-required-asterisk">*</span></label>
+<!-- <label class="ss-q-help" for="entry_0"></label> -->
+<input type="text" name="entry.0.single" value="" class="ss-q-short" id="entry_0"><!-- </div></div></div> -->
+<br> <!-- <div class="errorbox-good">
+<div class="ss-item ss-item-required ss-text"><div class="ss-form-entry"> --><label class="ss-q-title" for="entry_1">Your email address: 
+<span class="ss-required-asterisk">*</span></label>
+<label class="ss-q-help" for="entry_1"></label>
+<input type="text" name="entry.1.single" value="" class="ss-q-short" id="entry_1"><!-- </div></div></div> -->
+<br> <!-- <div class="errorbox-good">
+<div class="ss-item ss-item-required ss-text"><div class="ss-form-entry"> --><label class="ss-q-title" for="entry_3">Your location:
+<span class="ss-required-asterisk">*</span></label>
+<label class="ss-q-help" for="entry_3"></label>
+<input type="text" name="entry.3.single" value="" class="ss-q-short" id="entry_3"><!-- </div></div></div> -->
+<br> <!-- <div class="errorbox-good">
+<div class="ss-item ss-item-required ss-paragraph-text"><div class="ss-form-entry"> --><label class="ss-q-title" for="entry_2">Your story:
+<span class="ss-required-asterisk">*</span></label>
+<label class="ss-q-help" for="entry_2"></label>
+<textarea name="entry.2.single" rows="4" cols="80" class="ss-q-long" id="entry_2"></textarea><!-- </div></div></div> -->
+<br><br>
+<input type="hidden" name="pageNumber" value="0">
+<input type="hidden" name="backupCache" value="">
+
+
+<div class="ss-item ss-navigate"><div class="ss-form-entry">
+<button type="submit" name="submit" value="Submit">Submit</button>
+</div></div></form></div>
